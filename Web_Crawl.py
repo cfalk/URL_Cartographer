271bf45ff68a564070167fa0c01e7fd9d8f3b25d
Casey Falk <caseyfalk94@gmail.com>
2013-09-20 14:51:44 -0400

    Starting to strip data. Needs refinement.

--- Web_Crawl.py
+++ Web_Crawl.py
@@ -1,4 +1,17 @@
 import urllib2
 
-def search_web(key_word):
-	pass
+def strip_innards(raw_data):
+	#Two results for "<t>Hello</t> <f>World</f>"
+	###Needs to reference tags!
+	return raw_data[raw_data.index(">")+1: raw_data.index("</")1]
+
+def search_web(key_word, seed):
+	orig_seed = urllib2.urlopen(seed)
+	raw_data = orig_seed.read()
+
+	#Remove anything that isn't interesting to the content of the page.
+	body_data = raw_data[0: raw_data.index("/body>")+8]
+	print body_data
+
+print strip_innards("<hello>world</hello>")
+search_web("something", "http://www.pythonforbeginners.com/python-on-the-web/how-to-use-urllib2-in-python/")
